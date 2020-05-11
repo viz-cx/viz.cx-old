8387cdad7027a05c1905d2c3960749d3155d18d3
Vladimir Babin <vovababin@gmail.com>
2020-05-11 12:54:21 +0500

    Add send award function

--- src/script/pages/app-award.ts
+++ src/script/pages/app-award.ts
@@ -1,9 +1,11 @@
 import { LitElement, css, html, customElement } from 'lit-element';
 
-declare var viz: any;
+declare let viz: any;
 
 @customElement('app-award')
 export class AppAward extends LitElement {
+
+    private defaultDivider = 20;
 
     static get styles() {
         return css`
@@ -15,7 +17,41 @@ export class AppAward extends LitElement {
     }
 
     firstUpdated() {
-        this.shadowRoot?.querySelector('#viz')?.value = this.shadowRoot?.querySelector('#energy')?.value;
+        let vizContainer = this.shadowRoot?.querySelector('#viz');
+        let energyInput = this.shadowRoot?.querySelector('#energy') as HTMLInputElement;
+        if (energyInput) {
+            this.getEnergy().then(value => {
+                let maxAttr = document.createAttribute('max');
+                maxAttr.value = value.toString();
+                energyInput.attributes.setNamedItem(maxAttr);
+                let valueAttr = document.createAttribute('value');
+                valueAttr.value = (value / this.defaultDivider).toString();
+                energyInput.attributes.setNamedItem(valueAttr);
+                if (vizContainer) {
+                    vizContainer.innerHTML = energyInput.value + '%';
+                }
+            }, rejected => {
+                console.log(rejected);
+                if (vizContainer) {
+                    vizContainer.innerHTML = energyInput.value + '%';
+                }
+            });
+        }
+
+        if (!this.getIsChangeble()) {
+            let receiverInput = this.shadowRoot?.querySelector('input#receiver');
+            if (receiverInput) {
+                let readOnlyAttr = document.createAttribute('readOnly');
+                readOnlyAttr.value = 'readOnly';
+                receiverInput.attributes.setNamedItem(readOnlyAttr);
+            }
+            let memoInput = this.shadowRoot?.querySelector('input#memo');
+            if (memoInput) {
+                let readOnlyAttr = document.createAttribute('readOnly');
+                readOnlyAttr.value = 'readOnly';
+                memoInput.attributes.setNamedItem(readOnlyAttr);
+            }
+        }
     }
 
     render() {
@@ -23,20 +59,20 @@ export class AppAward extends LitElement {
             <p>
                 <label>
                     <span>Получатель:</span>
-                    <input type="text" name="awardee" value="${this._getAwardee()}" />
+                    <input id="receiver" name="receiver" type="text" value="${this.getReceiver()}" />
                 </label>
             </p>
             <p>
                 <label>
                     <span>Энергия:</span>
-                    <input id="energy" name="energy" type="range" min="1" max="${this._getEnergy()}" step="1" value="${this._getEnergy() / 10}" @change=${this.updateEnergy} />
-                    <input id="viz" value=""></span>
+                    <input id="energy" name="energy" type="range" min="0.01" step="0.01" @change=${this.updateEnergy} @input=${this.updateEnergy} />
+                    <span id="viz"></span>
                 </label>
             </p>
             <p>
                 <label>
                     <span>Пояснение:</span>
-                    <input type="text" name="memo" value="${this._getMemo()}"  />
+                    <input id="memo" name="memo" type="text" value="${this.getMemo()}"  />
                 </label>
             </p>
             <p>
@@ -45,28 +81,75 @@ export class AppAward extends LitElement {
         `;
     }
 
-    updateEnergy(elem: any) {
-        this.shadowRoot?.querySelector('#viz')?.value = elem.srcElement.value;
+    private updateEnergy(event: any): void {
+        let vizContainer = this.shadowRoot?.querySelector('#viz');
+        if (vizContainer) {
+            vizContainer.innerHTML = event.srcElement.value + '%';
+        }
     }
 
-    sendAward(): void {
-        alert(123);
+    private sendAward(): void {
+        let initiator: string, wif: string; // TODO: Get from storage
+
+        let receiverInput = this.shadowRoot?.querySelector('input#receiver') as HTMLInputElement;
+        if (!receiverInput || receiverInput.value.trim().length === 0) {
+            console.log('Receiver is not valid');
+            return;
+        }
+        let receiver = receiverInput.value.trim();
+
+        let energyInput = this.shadowRoot?.querySelector('input#energy') as HTMLInputElement;
+        if (!energyInput || energyInput.value.trim().length === 0) {
+            console.log('Energy is not valid');
+            return;
+        }
+        let energy = Number(energyInput.value.trim()) * 100;
+
+        let memoInput = this.shadowRoot?.querySelector('input#memo') as HTMLInputElement;
+        if (!memoInput || memoInput.value.trim().length === 0) {
+            console.log('Memo is not valid');
+            return;
+        }
+        let memo = memoInput.value.trim();
+        
+        let custom_sequence = 0;
+        let beneficiaries: string[] = [];
+
+        viz.broadcast.award(wif, initiator, receiver, energy, custom_sequence, memo, beneficiaries, function (err: any, result: any) {
+            console.log(err, result)
+        });
     }
 
-    _getEnergy(): number {
-        return 5450 / 100;
+    private async getEnergy(): Promise<number> {
+        let accounts: string[] = [];
+        return new Promise(resolve => {
+            viz.api.getAccounts(accounts, function(err: any, result: any) {
+                var minEnergy = 10000;
+                if (!err && result) {
+                    for (var account of result) {
+                        let energy: number = account['energy'];
+                        if (energy && energy < minEnergy) {
+                            minEnergy = energy;
+                        }
+                    }
+                } else {
+                    minEnergy = 0;
+                }
+                resolve(minEnergy / 100);
+            });
+        });
     }
 
-    _getMemo(): string {
-        return new URLSearchParams(location.search).get('link') || '';
+    private getMemo(): string {
+        return new URLSearchParams(location.search).get('memo') || '';
     }
 
-    _getAwardee(): string {
+    private getReceiver(): string {
         return new URLSearchParams(location.search).get('user') || '';
     }
 
-    _getChangable(): boolean {
+    private getIsChangeble(): boolean {
         let changeable = (new URLSearchParams(location.search).get('changeable') || '') == 'true';
-        return changeable || this._getMemo().length == 0 || this._getAwardee().length == 0;
+        return changeable || this.getMemo().length == 0 || this.getReceiver().length == 0;
     }
 }
